c52ef334ffd91a4be2b1787c43bfbce113501771
Edith707 <edith.lopezhuerta@gmail.com>
2018-07-03 12:38:55 -0500

    corriendo el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, Router} from "@angular/router";
+import { RouterModule, Routes} from "@angular/router";
 import { AboutComponent } from './about/about.component';
 import { ContactComponent} from "./contact/contact.component";
 import { GalleryComponent} from "./gallery/gallery.component";
 
 const routes:Routes =[
+
   {path:"about", component: AboutComponent},
   {path:"contact", component: ContactComponent},
   {path:"gallery", component: GalleryComponent}
